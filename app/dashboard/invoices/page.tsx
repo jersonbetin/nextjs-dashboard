673c6452259b665e9ade56dd40076db29fd49679
jbetinpantoja <jbetin@blossom.net>
2023-11-02 14:38:00 -0500

    init

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return <p>Invoices</p>;
+};
+
+Page.getInitialProps = async () => {
+  return {};
+};
+
+export default Page;
---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,15 @@
+import SideNav from "@/app/ui/dashboard/side-nav";
+import { ReactNode } from "react";
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="flex flex-col h-screen md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
